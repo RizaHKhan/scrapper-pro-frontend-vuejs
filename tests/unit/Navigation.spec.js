9c5ac601ad44319554eeeade43492fc343696443
khanr <khanriza@gmail.com>
2020-11-28 22:17:37 -0500

    adjustments to Navigation tests

--- tests/unit/Navigation.spec.js
+++ tests/unit/Navigation.spec.js
@@ -6,45 +6,61 @@ const user = {
   namespaced: true,
   state: () => ({
     _id: "",
-    username: "khanr",
-    token: "",
+    username: "",
+    token: ""
   }),
   getters: {
-    loggedIn: (state) => (state.token ? true : false),
-  },
+    loggedIn: state => (state.token ? true : false)
+  }
 };
 
 const store = createStore({
   modules: {
-    user,
-  },
+    user
+  }
 });
 
 describe("Navigation.vue", () => {
   it("renders Scrapper-Pro logo verbiage on mount", () => {
     const wrapper = mount(Navigation, {
       global: {
-        plugins: [store],
-      },
+        plugins: [store]
+      }
     });
-    console.log(wrapper.html());
     expect(wrapper.html().includes("Scrapper-Pro")).toBe(true);
   });
 
   it("renders login form if not logged in", () => {
     const wrapper = mount(Navigation, {
       global: {
-        plugins: [store],
-      },
+        plugins: [store]
+      }
     });
-    expect(wrapper.findComponent("form").exists()).toBe(true);
+    expect(wrapper.html().includes("form")).toBe(true);
   });
 
   it("renders logout button if logged in", () => {
+    const user = {
+      namespaced: true,
+      state: () => ({
+        _id: "",
+        username: "",
+        token: "2314"
+      }),
+      getters: {
+        loggedIn: state => (state.token ? true : false)
+      }
+    };
+
+    const store = createStore({
+      modules: {
+        user
+      }
+    });
     const wrapper = mount(Navigation, {
       global: {
-        plugins: [store],
-      },
+        plugins: [store]
+      }
     });
     expect(wrapper.html().includes("Logout")).toBe(true);
   });
